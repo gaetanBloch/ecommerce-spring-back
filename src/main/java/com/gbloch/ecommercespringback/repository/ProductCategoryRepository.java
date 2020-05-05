e9393e8a610d611dfd61b60a86641a7892f06289
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-05 22:37:32 +0200

    Added Annotation to The ProductCategoryRepository to define the values in json and in the url path

--- src/main/java/com/gbloch/ecommercespringback/repository/ProductCategoryRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductCategoryRepository.java
@@ -1,12 +1,13 @@
 package com.gbloch.ecommercespringback.repository;
 
+import com.gbloch.ecommercespringback.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Gaëtan Bloch
  * Created on 05/05/2020
  */
-@Repository
-public interface ProductCategoryRepository extends JpaRepository<ProductCategoryRepository, Long> {
+@RepositoryRestResource(collectionResourceRel = "product_categories", path = "product-categories")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
@@ -2,12 +2,10 @@ package com.gbloch.ecommercespringback.repository;
 
 import com.gbloch.ecommercespringback.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author Gaëtan Bloch
  * Created on 05/05/2020
  */
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
