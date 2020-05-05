dd1a1b60119228608aeb088c2c7e8c6b9442e127
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-05 22:52:03 +0200

    Made the Products readonly i.e. only possible to use GET requests

---
+++ src/main/java/com/gbloch/ecommercespringback/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.gbloch.ecommercespringback.config;
+
+import com.gbloch.ecommercespringback.model.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+/**
+ * @author Gaëtan Bloch
+ * Created on 05/05/2020
+ */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] unsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        // Disable HTTP methods for products: POST, PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metadata, httpMethods)
+                        -> httpMethods.disable(unsupportedMethods)))
+                .withCollectionExposure(((metadata, httpMethods)
+                        -> httpMethods.disable(unsupportedMethods)));
+
+    }
+}
