34329f616ee378fcb37e6d4d18af6f65961db230
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-05 19:12:19 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/gbloch/ecommercespringback/EcommerceSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.gbloch.ecommercespringback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSpringBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gbloch/ecommercespringback/EcommerceSpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbloch.ecommercespringback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
