75f98e0246c4e43e22e24f550eda6cd25549b46e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-05 22:04:37 +0200

    Added annotations to for mapping columns in the Product Class

--- src/main/java/com/gbloch/ecommercespringback/model/Product.java
+++ src/main/java/com/gbloch/ecommercespringback/model/Product.java
@@ -1,6 +1,8 @@
 package com.gbloch.ecommercespringback.model;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -18,14 +20,38 @@ import java.util.Date;
 @Entity
 @Table(name="product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "sku")
     private String sku;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "active")
     private boolean active;
+
+    @Column(name = "units_in_stock")
     private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 }
