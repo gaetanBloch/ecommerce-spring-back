8581293d6693fd44ea0dc207f18d6e16aad96f92
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-06 12:04:55 +0200

    Added support for swagger Spring Rest Data

---
+++ src/main/java/com/gbloch/ecommercespringback/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.gbloch.ecommercespringback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
+
+/**
+ * @author Gaetan Bloch
+ * Created on 15/04/2020
+ */
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        // Default configuration
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+
+        Contact contact = new Contact("Gaëtan Bloch",
+                "https://github.com/gaetanBloch",
+                "gaetan.bloch@gmail.com");
+
+        return new ApiInfo(
+                "REST API Documentation",
+                "Spring REST API Back-End for the E-commerce Application",
+                "1.0",
+                "Terms of Service",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+}
