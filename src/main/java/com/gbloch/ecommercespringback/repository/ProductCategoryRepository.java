2e03764b1388fa9f4b77573cca62e179c601914c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-06 14:25:25 +0200

    Add Cross Origin for repositories

--- src/main/java/com/gbloch/ecommercespringback/repository/ProductCategoryRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductCategoryRepository.java
@@ -3,11 +3,13 @@ package com.gbloch.ecommercespringback.repository;
 import com.gbloch.ecommercespringback.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author Gaëtan Bloch
  * Created on 05/05/2020
  */
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "product_categories", path = "product-categories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.gbloch.ecommercespringback.repository;
 
 import com.gbloch.ecommercespringback.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author Gaëtan Bloch
  * Created on 05/05/2020
  */
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
