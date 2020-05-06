378999ad3b08b225acc457d8c550c8a1f6bd7aaa
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-06 23:31:34 +0200

    Added Find Product By Category Id Endpoint

--- src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.gbloch.ecommercespringback.repository;
 
 import com.gbloch.ecommercespringback.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Gaëtan Bloch
@@ -10,4 +13,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  */
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@RequestParam Long id, Pageable pageable);
 }
