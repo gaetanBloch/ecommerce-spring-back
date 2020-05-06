766a830ea5ebf13782719498f3aa796ff47abd8b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-06 12:31:06 +0200

    Fixed support for Swagger documentation

--- src/main/java/com/gbloch/ecommercespringback/config/SwaggerConfig.java
+++ src/main/java/com/gbloch/ecommercespringback/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.gbloch.ecommercespringback.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,9 +18,10 @@ import java.util.ArrayList;
  * @author Gaetan Bloch
  * Created on 15/04/2020
  */
+@Configuration
 @EnableSwagger2WebMvc
 @Import(SpringDataRestConfiguration.class)
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
