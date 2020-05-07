19d67cf3c04da4c71f4ce0704ebfe4762133e102
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-07 12:10:17 +0200

    Changed the rel string

--- src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @RestResource(path = "category-id", rel = "category_id")
+    @RestResource(path = "category-id", rel = "category-id")
     Page<Product> findByCategoryId(@RequestParam Long id, Pageable pageable);
 }
