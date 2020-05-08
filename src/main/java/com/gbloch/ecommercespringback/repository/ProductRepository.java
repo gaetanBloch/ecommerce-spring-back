0bebfda7118d1bc7b99d5f4ab72e3946a6a1588e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-08 17:10:53 +0200

    Modified Repository classes

--- src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
+++ src/main/java/com/gbloch/ecommercespringback/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.gbloch.ecommercespringback.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Gaëtan Bloch
  * Created on 05/05/2020
  */
+@Repository
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(path = "products", collectionResourceRel = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @RestResource(path = "category-id", rel = "category-id")
     Page<Product> findByCategoryId(@RequestParam Long id, Pageable pageable);
